872136ffbbd481c54a59bd73f51508251b2cbf26
enricomartins018 <enrico.martins018@gmail.com>
2022-08-29 21:55:58 -0300

    feat: Formulário de nascimento, cpf e renda mensal finalizado.

--- src/Components/FormFinal/FormFinal.jsx
+++ src/Components/FormFinal/FormFinal.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import S from './FormFinal.module.css'
 
 const FormFinal = () => {
   return (
     <div>
-        <h1>Hello Final</h1>
+      <form className={S.form}>
+        <div className={S.containerForm}>
+          <h4 className={S.titleForm}>Insira seus dados pessoais</h4>
+          <div className={S.cep}>
+            <label>Data de Nascimento</label>
+            <input className={S.inputPattern} type="date" name="date" id="date" />
+          </div>
+          <div className={S.endereco}>
+            <label>CPF ou CNPJ</label>
+            <input className={S.inputPattern} type="text" name="cpf" id="cpfOuCnpj" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})" />
+          </div>
+          <div className={S.endereco}>
+            <label>Renda Mensal</label>
+            <input className={S.inputPattern} type="text" name="text" id="rendaMensal" />
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
