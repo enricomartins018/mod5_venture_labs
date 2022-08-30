8f055e04e9aa15dddd5977482868a13f60012633
enricomartins018 <enrico.martins018@gmail.com>
2022-08-29 23:47:10 -0300

    feat: Router criada e configurada.

---
+++ src/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cadastrar from './Pages/Cadastrar';
+import Clientes from './Pages/Clientes';
+
+const Router = () => {
+    return (
+        <BrowserRouter className="App">
+            <Routes>
+                <Route path='/' element={<Cadastrar />}/>
+                <Route path='/Clientes' element={<Clientes/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
