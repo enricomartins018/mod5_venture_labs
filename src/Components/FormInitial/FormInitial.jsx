d75d65b6f891bb135211ff51e2c4466ad907882a
enricomartins018 <enrico.martins018@gmail.com>
2022-08-29 21:54:39 -0300

    feat: Form inicial finalizado.

--- src/Components/FormInitial/FormInitial.jsx
+++ src/Components/FormInitial/FormInitial.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import S from './FormInitial.module.css'
 
 const FormInitial = () => {
   return (
     <div>
-      <h1>Hello Initial</h1>
+      <form className={S.form}>
+        <div className={S.containerForm}>
+          <h4 className={S.titleForm}>Faça seu cadastro</h4>
+          <div className={S.nome}>
+            <label>Nome</label>
+            <input className={S.inputPattern} type="text" name="name" id="" />
+          </div>
+          <div className={S.sobrenome}>
+            <label>Sobrenome</label>
+            <input className={S.inputPattern} type="text" name="name" id="" />
+          </div>
+          <div className={S.email}>
+            <label>E-mail</label>
+            <input className={S.inputPattern} type="email" name="email" id="" />
+          </div>
+          <div className={S.telefone}>
+            <label>Telefone</label>
+            <input className={S.inputPattern} type="tel" name="tel" id="" />
+          </div>
+        </div>
+
+      </form >
     </div>
   )
 }
